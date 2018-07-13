5045f384c1155fa760c647c4f48ea21abbd7c239
Gregor Cox <gregorcox@gmail.com>
2018-07-13 11:05:38 +0100

    sets up league class

--- src/main/java/models/League.java
+++ src/main/java/models/League.java
@@ -1,4 +1,46 @@
 package models;
 
+import javax.persistence.*;
+import java.util.List;
+
+
+@Entity
+@Table(name = "league")
 public class League {
+
+    private int id;
+    private String name;
+    private List<Team> teams;
+
+    public League(String name, List<Team> teams) {
+        this.name = name;
+        this.teams = teams;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(List<Team> teams) {
+        this.teams = teams;
+    }
 }
