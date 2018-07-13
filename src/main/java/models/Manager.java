7ed5a397ec51364992132c300101dab9a8f63186
Gregor Cox <gregorcox@gmail.com>
2018-07-13 11:18:04 +0100

    sets up manager class

--- src/main/java/models/Manager.java
+++ src/main/java/models/Manager.java
@@ -1,6 +1,58 @@
 package models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "managers")
 public class Manager {
 
+    private int id;
+    private String firstName;
+    private String lastName;
+    private Team team;
 
+    public Manager(){}
+
+    public Manager(String firstName, String lastName, Team team) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.team = team;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "first_name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "last_name")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 }
