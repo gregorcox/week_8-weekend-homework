282912850131ce5c6ef6e5669382c21cf393f320
Gregor Cox <gregorcox@gmail.com>
2018-07-13 11:13:34 +0100

    sets up teams class

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -1,4 +1,56 @@
 package models;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "teams")
 public class Team {
+
+    private int id;
+    private String name;
+    private List<Player> players;
+    private Manager manager;
+
+    public Team(){}
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
 }
